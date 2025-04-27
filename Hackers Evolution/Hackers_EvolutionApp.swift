22b6f293be639902f4bbfd1311953aecbe16db8a
Eric Baur <baurfamily@gmail.com>
2025-04-26 23:18:03 -0700

    Initial Commit

---
+++ Hackers Evolution/Hackers_EvolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackers_EvolutionApp.swift
+//  Hackers Evolution
+//
+//  Created by Eric Baur on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Hackers_EvolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hackers EvolutionTests/Hackers_EvolutionTests.swift
@@ -0,0 +1,16 @@
+//
+//  Hackers_EvolutionTests.swift
+//  Hackers EvolutionTests
+//
+//  Created by Eric Baur on 4/26/25.
+//
+
+import Testing
+
+struct Hackers_EvolutionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
